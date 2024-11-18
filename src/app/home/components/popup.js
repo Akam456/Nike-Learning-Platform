91fdb47b8622818c1f493871f985c0353524ea42
jawtopi <jasunlee24@gmail.com>
2024-11-17 23:59:16 -0800

    add features to home popup

--- src/app/home/components/popup.js
+++ src/app/home/components/popup.js
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Image from 'next/image';
 
 const PopUp = ({ topTitle, title, rating, closePopUp }) => {
+    const [completedLevels, setCompletedLevels] = useState({}); // State to track clicked levels
+
     let content = {
         title: title,
         description: 'Predict future demand using quantitative and qualitative methods, leveraging historical data, market trends and advanced analytics.',
@@ -13,38 +16,35 @@ const PopUp = ({ topTitle, title, rating, closePopUp }) => {
         topTitle: topTitle,
         totalLevel: 3,
     };
-    const handleLevelClick = () => {
-        window.open("https://rise.articulate.com/share/QXpnKWM8HvPXmDs8Z0_UlXtziEsZYx7G", "_blank");
+
+    const handleLevelClick = (index) => {
+        // Mark the level as completed
+        setCompletedLevels((prev) => ({
+            ...prev,
+            [index]: true,
+        }));
     };
+
     return (
         <div className="fixed inset-0 w-full h-full bg-black bg-opacity-50 z-50 flex justify-center items-center">
             <div className="bg-black p-4 w-11/12 max-w-6xl h-auto max-h-[90vh] rounded-lg relative">
                 
                 {/* Top Title */}
                 <div className="relative text-white p-8 rounded-t-lg h-[20vh] flex justify-start items-end border border-gray-500">
-    
-                    {/* Background Image */}
                     <div
                         className="absolute inset-0 bg-cover bg-center"
                         style={{ backgroundImage: `url('/images/popup-image.jpeg')` }}
                     ></div>
-    
-                    {/* Transparent Overlay */}
                     <div className="absolute inset-0 bg-black bg-opacity-50"></div>
-
-                    {/* Close Button (X) */}
                     <button
                         onClick={closePopUp}
                         className="absolute top-4 right-4 text-white text-2xl font-bold focus:outline-none"
                     >
                         &times;
                     </button>
-
-                    {/* Title Content */}
                     <div className="relative z-10">
                         <h2 className="text-4xl md:text-6xl font-semibold">{content.title}</h2>
                     </div>
-
                 </div>
 
                 {/* Course Content */}
@@ -53,13 +53,12 @@ const PopUp = ({ topTitle, title, rating, closePopUp }) => {
                         <div className="text-lg text-white leading-[30px] font-helveticaNeue text-[22px] w-full md:w-3/4">
                             {content.description}
                         </div>
-                        {/* Test button */}
                         <div className="pl-0 mb-32 pt-2">
-                            <a href = "https://docs.google.com/document/d/1gyES-9cwrY05MseRj84ueVmk28kyEa4u5_5R4DghiCw/edit" target="blank">
-                            <button className="ml-0 mr-4 w-[120px] h-[50px] bg-white text-black text-[24px] font-semibold rounded-md">
-                                Test
-                                <span className="ml-2">&gt;</span>
-                            </button>
+                            <a href="https://docs.google.com/document/d/1gyES-9cwrY05MseRj84ueVmk28kyEa4u5_5R4DghiCw/edit" target="blank">
+                                <button className="ml-0 mr-4 w-[120px] h-[50px] bg-white text-black text-[24px] font-semibold rounded-md">
+                                    Test
+                                    <span className="ml-2">&gt;</span>
+                                </button>
                             </a>
                         </div>
                     </div>
@@ -67,7 +66,6 @@ const PopUp = ({ topTitle, title, rating, closePopUp }) => {
                         <h3 className="mb-4"> 
                             Skill Area: <span className="text-red-400 font-semibold">{content.topTitle}</span> 
                         </h3>
-                        {/* Rating */}
                         <p className="mb-4">
                             Average Rating: <span className="text-red-400 font-semibold">★ {content.rating}</span>
                         </p>
@@ -77,31 +75,57 @@ const PopUp = ({ topTitle, title, rating, closePopUp }) => {
                     </div>
                 </div>
 
-
                 {/* Levels */}
                 <div className="space-x-2 md:space-x-12 ml-4 mr-4 flex md:flex-row justify-center p-2 pb-[2rem] mt-6">
                     {content.levels.map((level, index) => (
                         <div
                             key={index}
-                            className="flex flex-col space-y-8 justify-between items-center p-5 rounded-md w-full md:w-[30%] transition duration-300 hover:opacity-50"
-                            style={{ backgroundColor: level.backgroundColor, cursor: 'pointer'}}
-                            onClick = {handleLevelClick}
+                            className={`flex flex-col text-white space-y-8 justify-between items-center p-5 rounded-md w-full md:w-[30%] transition duration-300 ${
+                                completedLevels[index]
+                                    ? 'bg-[#5a452b] text-opacity-50'
+                                    : ''
+                            }`}
+                            style={{
+                                backgroundColor: level.backgroundColor,
+                                opacity: completedLevels[index] ? 0.5:1,
+                                cursor: 'pointer',
+                                color: completedLevels[index] ? '#ffffff' : '',
+                            }}
+                            onClick={() => handleLevelClick(index)}
                         >
-                            <div className="text-white text-3xl md:text-4xl font-semibold relative" style={{ left: '0rem' }}>
+                            <div
+                                className={`text-3xl flex justify-between md:text-4xl font-semibold relative ${
+                                    completedLevels[index] ? 'text-opacity-50' : ''
+                                }`}
+                            >
                                 {level.name}
+                                {completedLevels[index] ? (
+                                    <Image
+                                    src='/images/sample-tick.svg'
+                                    width={23}
+                                    height={23}
+                                    className='absolute top-0 right-0'
+                                />): (<></>)}
+                                
                             </div>
-                            <div className="flex space-x-1 ml-[-9.375rem] mt-2">
-                                <div className="w-6 h-6">
-                                    <img src="/images/clock.png" alt="clock icon"/>
+                            <div className="flex items-center justify-between gap-x-20">
+                                <div className="flex items-center gap-x-2">
+                                    <div className="w-6 h-6">
+                                        <img src="/images/clock.png" alt="clock icon" />
+                                    </div>
+                                    <div className="text-black text-xl font-semibold">
+                                        {level.time}
+                                    </div>
                                 </div>
-                                <div className="text-black text-xl font-semibold">
-                                    {level.time}
-                                </div>
+                                {completedLevels[index] ? (
+                                    <div className="text-white font-bold text-lg">Complete</div>
+                                ) : (
+                                    <Image src="/images/nike-points.svg" width={70} height={32} />
+                                )}
                             </div>
                         </div>
                     ))}
                 </div>
-                
             </div>
         </div>
     );
